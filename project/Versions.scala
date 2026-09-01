8194763528388ae81eb083eae5e2111e2d223c39
Mark Canlas <github@htmlism.com>
2026-09-01 17:05:09 -0400

    weaver 0.13.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -5,5 +5,5 @@ object Versions {
   val http4s          = "0.23.34"
   val http4sScalatags = "0.25.3"
   val slf4jSimple     = "2.0.18"
-  val weaver          = "0.8.4"
+  val weaver          = "0.13.0"
 }
